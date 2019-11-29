f39ef09c4cd091f420e6f4b88b22bc2f05128f7b
maratkurbangaleev <mkurbangaleev@gmail.com>
2019-11-29 09:59:01 +0300

    Update hw3.sql

--- hw3.sql
+++ hw3.sql
@@ -13,3 +13,6 @@ REFERENCES public.listing_task ("ID");
 ALTER TABLE public.quotes_task
 ADD CONSTRAINT fr_key_2 FOREIGN KEY ("ISIN")
 REFERENCES public.bond_description_task ("ISIN, RegCode, NRDCode");
+
+-- Комментарий:
+-- похоже на правду, нужно проверить после создания таблиц.
